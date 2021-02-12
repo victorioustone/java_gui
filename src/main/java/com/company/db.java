ce8fba1e7b061373f3dd7099f03f19468f7a8d37
victorioustone <ms.vicstone@gmail.com>
2021-02-12 21:30:22 +0300

    ++

--- src/main/java/com/company/db.java
+++ src/main/java/com/company/db.java
@@ -4,12 +4,26 @@ import java.sql.*;
 
 public class db {
 
-    public int Connect(String LOGIN, String PASSWORD) throws SQLException {
+    public int Connect(String LOGIN, String PASSWORD){
         String URL = "jdbc:postgresql://127.0.0.1:5432/rubber_duck_db";
-        Connection connection = null;
+        Connection connection;
         try {
             connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
             System.out.println("Database was connected");
+            Statement statement;
+            try {
+                statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM duckee");
+                while (resultSet.next()) {
+                    System.out.print(resultSet.getString(1) + "  ");
+                    System.out.print(resultSet.getString(2) + "  ");
+                    System.out.print(resultSet.getString(3) + "  ");
+                    System.out.print(resultSet.getString(4) + "  ");
+                    System.out.println();
+                }
+            } catch (SQLException e){
+                System.out.println("query failed");
+            }
             return 0;
         } catch (SQLException sqlException) {
             System.out.println("Connection failed");
